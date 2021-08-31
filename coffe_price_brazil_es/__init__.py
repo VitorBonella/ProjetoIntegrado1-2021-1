9f31cb3a0afbb308b6ae3c6d3b9398f75bb5c4ab
Vitor Bonella <vitorb.bonella36@gmail.com>
2021-08-31 15:23:06 -0300

    API dev

---
+++ coffe_price_brazil_es/__init__.py
@@ -0,0 +1,4 @@
+from coffe import Coffe
+
+coffee = Coffe()
+print(coffee.getTable().columns)
---
+++ coffe_price_brazil_es/get_coffe_csv_file.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+
+
+def get_csv_file():
+    page_url = "https://www.cecafe.com.br/indicadores-de-mercado/precos-vitoria/"
+    html = requests.get(page_url)
+
+    soup = BeautifulSoup(html.content, "html.parser")
+
+    tbl = soup.find("table")
+
+    body = tbl.find_all("tr")
+
+    body_rows = body[1:]
+
+    headings = ["DATA", "ARABICA RUIM", "ARABICA BOM", "CONILLON"]
+
+    all_rows = []
+    for row_num in range(len(body_rows)):
+        row = []
+        for row_item in body_rows[row_num].find_all("td"):
+
+            info = row_item.text
+
+            row.append(info)
+
+        all_rows.append(row)
+
+    df = pd.DataFrame(data=all_rows, columns=headings)
+
+    df.to_csv("price_table.csv", index=False)
+
+    return df
