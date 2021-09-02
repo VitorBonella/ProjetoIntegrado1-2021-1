1c33a5ded593b2610152dc4e203b960c3fc3cac6
Vitor Bonella <vitorb.bonella36@gmail.com>
2021-09-02 15:02:40 -0300

    base creation

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -2,8 +2,6 @@ from app import app
 from flask import render_template
 
 
-@app.route("/<user>")
-@app.route("/", defaults={"user": None})
-
-def index(user):
-    return render_template("base.html", data=[1, 2, 3, 4, 5, 6])
+@app.route("/")
+def index():
+    return render_template("index.html")
