f4acbb6a397126a5b85816d41fa7c87028519aa4
Vitor Bonella <vitorb.bonella36@gmail.com>
2021-09-02 15:14:26 -0300

    bootstrap dev

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -2,6 +2,12 @@ from app import app
 from flask import render_template
 
 
+@app.route("/index")
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
