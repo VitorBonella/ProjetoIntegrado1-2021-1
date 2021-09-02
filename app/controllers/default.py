4d2fac4a7c89ff8ab68b8fdaf7ee6dd828281818
Luis Câmara <57678124+LuisEduardoF@users.noreply.github.com>
2021-09-02 14:55:02 -0300

    Commit

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -4,5 +4,6 @@ from flask import render_template
 
 @app.route("/<user>")
 @app.route("/", defaults={"user": None})
+
 def index(user):
-    return render_template("index.html", user=user)
+    return render_template("base.html", data=[1, 2, 3, 4, 5, 6])
