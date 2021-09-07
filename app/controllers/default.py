b057255b998f17166bfbf66f8a1d43f118379ec7
Luis Câmara <57678124+LuisEduardoF@users.noreply.github.com>
2021-09-07 17:19:34 -0300

    Gráficos genéricos para os 3 tipos de café

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -38,7 +38,7 @@ def signup():
             flash("Registrado com sucesso", "success")
             return redirect("/")
         except exc.IntegrityError:
-            flash("Apelido já utilizado", "danger")
+            flash("Já existe uma conta com esses informações", "danger")
             return render_template('register.html', form=form)
 
         # return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
@@ -72,7 +72,7 @@ def announces():
 
 
 @app.route("/infocafe/geral")
-def graph_test():
+def graph_geral():
 
     # labels, values0, values1, values2 = coffee.get_prices_by_range("05/05/2020", "01/01/2022")
     # labels, values0, values1, values2 = coffee.get_prices_by_year(2019)
@@ -83,5 +83,37 @@ def graph_test():
     _, values2 = coffee.get_prices_by_type("CONILLON")
     values = [values0, values1, values2]
 
-    return render_template("graph.html", labels=labels, values=values)
+    return render_template("graph.html", labels=labels, values=values, types=["Arabica ruim", "Arabica bom", "Conillon"], colors=["rgb(0,220,255)", "rgb(0,50,255)", "rgb(63,255,0)"])
 
+@app.route("/infocafe/arabica_ruim")
+def graph_arabica_ruim():
+
+    # labels, values0, values1, values2 = coffee.get_prices_by_range("05/05/2020", "01/01/2022")
+    # labels, values0, values1, values2 = coffee.get_prices_by_year(2019)
+
+
+    labels, values0 = coffee.get_prices_by_type("ARABICA RUIM")
+    values = [values0]
+    return render_template("graph.html", labels=labels, values=values, types=["Arabica ruim"], colors=["rgb(0,220,255)"])
+
+@app.route("/infocafe/arabica_bom")
+def graph_arabica_bom():
+
+    # labels, values0, values1, values2 = coffee.get_prices_by_range("05/05/2020", "01/01/2022")
+    # labels, values0, values1, values2 = coffee.get_prices_by_year(2019)
+
+
+    labels, values0 = coffee.get_prices_by_type("ARABICA BOM")
+    values = [values0]
+    return render_template("graph.html", labels=labels, values=values, types=["Arabica bom"], colors=["rgb(0,50,255)"])
+
+@app.route("/infocafe/conillon")
+def graph_conillon():
+
+    # labels, values0, values1, values2 = coffee.get_prices_by_range("05/05/2020", "01/01/2022")
+    # labels, values0, values1, values2 = coffee.get_prices_by_year(2019)
+
+
+    labels, values0 = coffee.get_prices_by_type("CONILLON")
+    values = [values0]
+    return render_template("graph.html", labels=labels, values=values, types=["Conillon"], colors=["rgb(63,255,0)"])
