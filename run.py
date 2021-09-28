b41dea9488eda2563b2befed9010ea83774a1b07
Vitor Bonella <vitorb.bonella36@gmail.com>
2021-09-28 18:23:14 -0300

    locale

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import app
 import os
+import locale
 
 if __name__ == "__main__":
+    locale.setlocale(locale.LC_ALL,"pt-br")
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=False, host="0.0.0.0", port=port)
