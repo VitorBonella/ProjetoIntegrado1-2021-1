1972d547b58a377b9aed82a3b0b1f608150da71b
Vitor Bonella <vitorb.bonella36@gmail.com>
2021-09-28 18:58:16 -0300

    Desbugar infocafe

--- run.py
+++ run.py
@@ -3,6 +3,5 @@ import os
 import locale
 
 if __name__ == "__main__":
-    locale.setlocale(locale.LC_ALL, "pt_BR.UTF-8")
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=False, host="0.0.0.0", port=port)
